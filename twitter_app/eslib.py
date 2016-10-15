9422910a39dd5cec00a361d204011555a3c4dd1f
Cary Xing <chaw.hs@gmail.com>
2016-10-15 13:10:04 -0400

    Updated the mechanism to add tweet to elastic cloud 1. Use a different index for each hour. 2. Remove the indices that are older than 24 hours.

--- twitter_app/eslib.py
+++ twitter_app/eslib.py
@@ -6,7 +6,10 @@ ES_CONFIG_FILE = "./env.cfg"
 from elasticsearch import Elasticsearch, RequestsHttpConnection
 import ConfigParser
 import json
+from datetime import datetime
 
+
+INDEX_PREFIX = "twitter-"
 
 def json_pretty_print(json_obj):
     print json.dumps(json_obj, indent=4)
@@ -28,11 +31,55 @@ class ElasticSearchForTweets:
                            use_ssl=True,
                            connection_class=RequestsHttpConnection,
                            verify_certs=False)
+        self.hour = None
+        self.current_index = None
+        self.del_old_create_new_index()
 
     def add_tweet(self, tweet_body):
-        #indices = self.es.indices.get('twitter')
-        #json_pretty_print(indices)
-        self.es.index(index="twitter", doc_type="tweet", body=tweet_body)
+        if datetime.today().hour != self.hour:
+            del_old_create_new_index()
+        self.es.index(index=self.current_index, doc_type="tweet", body=tweet_body)
+
+
+    def del_old_create_new_index(self):
+        mappings = {
+            "tweet": {
+                "properties": {
+                    "id": {
+                        "type": "string"
+                    },
+                    "coordinates": {
+                        "type": "geo_point"
+                    },
+                    "timestamp_ms": {
+                        "type": "string"
+                    }
+                }
+            }
+        }
+        indices = self.es.indices.get(INDEX_PREFIX + "*")
+        indexSeqs = []
+        for index in indices:
+            indexSeqs.append(int(index.split('-')[1]))
+        indexSeqs.sort(reverse=True)
+
+        # remove indices that are older than 24 hours
+        for i in xrange(24, len(indexSeqs)):
+            self.es.indices.delete(index = INDEX_PREFIX + str(indexSeqs[i]))
+
+        if len(indexSeqs) > 0:
+            newSeq = indexSeqs[0] + 1
+        else:
+            newSeq = 0
+
+        newIndex = INDEX_PREFIX + str(newSeq)
+        self.es.indices.create(index = newIndex)
+        self.es.indices.put_mapping(index = newIndex, doc_type = "tweet", body = json.dumps(mappings))
+        print("New index %s is created."%(newIndex))
+
+        self.hour = datetime.today().hour
+        self.current_index = newIndex
+        
 
     def get_instance(self):
         return self.es
@@ -58,12 +105,39 @@ def est_debug():
         }
     }
     
-
     es = ElasticSearchForTweets().get_instance()
-    es.indices.delete("twitter")
-    es.indices.create(index="twitter")
-    es.indices.put_mapping(index="twitter", doc_type="tweet", body=json.dumps(mappings))
-    json_pretty_print(es.indices.get("twitter"))
+    #es.indices.delete("twitter")
+    #es.indices.create(index=INDEX_PREFIX+"2")
+    #es.indices.put_mapping(index="twitter", doc_type="tweet", body=json.dumps(mappings))
+    #json_pretty_print(es.indices.get("twitter*"))
+    #json_pretty_print(es.search("twitter*", body={"query": {"match_all": {}}}))
+
+    query = {
+        "sort" : [
+            {"timestamp_ms" : "desc"}
+        ],
+        "from" : 0, 
+        "size" : 250,
+        "query": {
+            "bool" : {
+                "must" : {
+                    "match_all" : {}
+                },
+
+                "filter" : {
+                    "geo_distance" : {
+                        "distance" : "120km",
+                        "coordinates" : "37,-122"
+                    }
+                }
+            }
+        }
+    }
+
+    #res = es.search(index = "twitter*", body = query)
+    #print("Got %d Hits:" % res['hits']['total'])
+    #for hit in res['hits']['hits']:
+    #    print("%s" % hit["_source"])
 
 
 # TODO: remove
